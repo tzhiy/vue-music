e0eb0dcbdae3d6ce543e86f9cc52dcf23bd62ad7
tzhiy <2570986081@qq.com>
2022-02-25 17:35:56 +0800

    feat: Tab 组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,31 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
-const routes = []
+import Recommend from '@/views/recommend'
+import Singer from '@/views/singer'
+import Toplist from '@/views/top-list'
+import Search from '@/views/search'
+const routes = [
+  {
+    path: '/',
+    // 默认指向 /recommend
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer
+  },
+  {
+    path: '/top-list',
+    component: Toplist
+  },
+  {
+    path: '/search',
+    component: Search
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
