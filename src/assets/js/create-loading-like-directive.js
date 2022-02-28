5d6b9215b0eb3223332f14c13b6e5f26f3efb286
tzhiy <2570986081@qq.com>
2022-02-28 19:26:59 +0800

    fix: 两个指令不能作用到同一个元素

--- src/assets/js/create-loading-like-directive.js
+++ src/assets/js/create-loading-like-directive.js
@@ -12,7 +12,13 @@ export default function createLoadingLikeDirective(Comp) {
       const app = createApp(Comp)
       // 组件的实例
       const instance = app.mount(document.createElement('div'))
-      el.instance = instance
+      // 获取组件的名称
+      const name = Comp.name
+      // 创建对象相应的属性
+      if (!el[name]) {
+        el[name] = {}
+      }
+      el[name].instance = instance
 
       // 传递给指令的动态参数
       const title = binding.arg
@@ -27,8 +33,9 @@ export default function createLoadingLikeDirective(Comp) {
     },
     updated(el, binding) {
       const title = binding.arg
+      const name = Comp.name
       if (typeof title !== 'undefined') {
-        el.instance.setTitle(title)
+        el[name].instance.setTitle(title)
       }
 
       // binding 的值改变时，进行判断
@@ -39,6 +46,7 @@ export default function createLoadingLikeDirective(Comp) {
   }
 
   function append(el) {
+    const name = Comp.name
     // 将挂载到的元素样式设置 postion 属性为非 static
     // getComputedStyle 用于获取元素属性
     const style = getComputedStyle(el)
@@ -46,11 +54,17 @@ export default function createLoadingLikeDirective(Comp) {
       addClass(el, relativeCls)
     }
     // el.instance 组件实例，el.instance.$el 组件 dom 对象
-    el.appendChild(el.instance.$el)
+    el.appendChild(el[name].instance.$el)
   }
 
   function remove(el) {
+    const name = Comp.name
     removeClass(el, relativeCls)
-    el.removeChild(el.instance.$el)
+    el.removeChild(el[name].instance.$el)
+    // Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.
+    // 由于在同一个元素上使用了 v-loading 和 v-no-result 两个指令
+    // v-no-result 创建的实例会覆盖掉 v-loading 创建的实例赋值的 el.instance
+    // 导致 v-loading 想要移除元素时找不到自己的实例对应的元素
+    // 这里将 el 添加一个维度，通过指令的名称来找到自己的实例对象
   }
 }
